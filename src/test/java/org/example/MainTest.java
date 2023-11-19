120ec8c199ad1c7d28a78d9779ab9c49d119e412
BurkovIA <burkov88@mail.ru>
2023-11-19 21:35:18 +0800

    update Tests

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,31 +1,24 @@
 package org.example;
-
-
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.io.*;
 import java.util.List;
-
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class MainTest {
+    MainTest() throws IOException, CsvException {
+    }
     @Test
     void testWriteString(){
         String json = "{\"lastName\":\"1256\",\"country\":\"U158SA\",\"age\":h}";
         boolean result = Main.writeString(json);
         Assert.assertTrue(result);
     }
-
     @Test
     void parseCSVtest(){
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
@@ -40,18 +33,30 @@ class MainTest {
         assertEquals(2, employees.size());
     }
     @Test
+    void Equal () throws IOException, CsvException {
+        CSVReader reader1 = new CSVReader(new FileReader("data.csv"));
+        List<String[]> Entries1 = reader1.readAll();
+
+        CSVReader reader2 = new CSVReader(new FileReader("test_data.csv"));
+        List<String[]> Entries2 = reader2.readAll();
+
+        for (int i = 0; i < Entries1.size(); i++) {
+            assertArrayEquals(Entries1.get(i), Entries2.get(i));
+        }
+    }
+    @Test
     void testListToJson() {
-        List<Employee> employee = List.of(
+
+        List<Employee> expectedEmployees = List.of(
                 new Employee(1, "John", "Smith", "USA", 25),
                 new Employee(2, "Ivan", "Petrov", "RU", 23)
         );
-        Type listType = new TypeToken<List<Employee>>() {
-        }.getType();
-        String exectedJson = new GsonBuilder().create().toJson(employee, listType);
 
-        String actualJson = Main.listToJson(employee);
+        String exectedJson = "[{\"id\":1,\"firstName\":\"John\",\"lastName\":\"Smith\",\"country\":\"USA\",\"age\":25}," +
+                "{\"id\":2,\"firstName\":\"Ivan\",\"lastName\":\"Petrov\",\"country\":\"RU\",\"age\":23}]";
+
+        String actualJson = Main.listToJson(expectedEmployees);
 
         Assertions.assertEquals(exectedJson, actualJson);
     }
-
 }
