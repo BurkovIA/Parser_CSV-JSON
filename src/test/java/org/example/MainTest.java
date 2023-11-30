f071a8c7b07a3207fc28f26c8bc58fa0c7279a30
BurkovIA <burkov88@mail.ru>
2023-11-30 19:51:44 +0800

    update test - 2

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -18,24 +18,14 @@ class MainTest {
         Assert.assertTrue(result);
     }
     @Test
-    void compareParserCSVwithTest () throws IOException, CsvException {
-        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-        String fileName = "test_data.csv";
-        try (FileWriter fileWriter = new FileWriter(fileName, true)) {
-            fileWriter.write("1,John,Smith,USA,25\n");
-            fileWriter.write("2,Inav,Petrov,RU,23");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
+    void compareParserCSVtest () throws IOException, CsvException {
+        String [][] Entries1 = {{"1","John","Smith","USA","25"},{"2","Inav","Petrov","RU","23"}};
+        
         CSVReader reader1 = new CSVReader(new FileReader("data.csv"));
-        List<String[]> Entries1 = reader1.readAll();
+        List<String[]> Entries2 = reader1.readAll();
 
-        CSVReader reader2 = new CSVReader(new FileReader("test_data.csv"));
-        List<String[]> Entries2 = reader2.readAll();
-
-        for (int i = 0; i < Entries1.size(); i++) {
-            assertArrayEquals(Entries1.get(i), Entries2.get(i));
+        for (int i = 0; i < Entries2.size(); i++) {
+            assertArrayEquals(Entries2.get(i), Entries1 [i]);
         }
     }
     @Test
